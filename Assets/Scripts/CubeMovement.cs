abdae25d842e0a63f1caec271aa2b3adf6c76c9c
Sophie Harms <harms_sophie@web.de>
2020-12-27 15:53:20 +0100

    Replaced static curObject by using Selection

--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEditor;
 
 public class CubeMovement : MonoBehaviour
 {
@@ -6,39 +7,40 @@ public class CubeMovement : MonoBehaviour
   static Vector3 a = new Vector3(0,0,-1.0f);
   static Vector3 s = new Vector3(1.0f,0,0);
   static Vector3 d = new Vector3(0,0,1.0f);
-    // Update is called once per frame
+    
     void Update()
     {
-      if(ObjectClicker.CurObject != null){
+      if(Selection.activeGameObject != null){
+         
       if (Input.GetKeyDown("w"))
       {
           //print("w was pressed");
-          if(Physics.Raycast(ObjectClicker.CurObject.transform.position, w, 1) == false){
-            ObjectClicker.CurObject.transform.position += w;
+          if(Physics.Raycast(Selection.activeGameObject.transform.position, w, 1) == false){
+                    Selection.activeGameObject.transform.position += w;
           }
       }
 
       if (Input.GetKeyDown("a"))
       {
           //print("a was pressed");
-          if(Physics.Raycast(ObjectClicker.CurObject.transform.position, a, 1) == false){
-            ObjectClicker.CurObject.transform.position += a;
+          if(Physics.Raycast(Selection.activeGameObject.transform.position, a, 1) == false){
+                    Selection.activeGameObject.transform.position += a;
           }
       }
 
       if (Input.GetKeyDown("d"))
       {
           //print("d was pressed");
-          if(Physics.Raycast(ObjectClicker.CurObject.transform.position, d, 1) == false){
-            ObjectClicker.CurObject.transform.position += d;
+          if(Physics.Raycast(Selection.activeGameObject.transform.position, d, 1) == false){
+                    Selection.activeGameObject.transform.position += d;
           }
       }
 
       if (Input.GetKeyDown("s"))
       {
           //print("s was pressed");
-          if(Physics.Raycast(ObjectClicker.CurObject.transform.position, s, 1) == false){
-            ObjectClicker.CurObject.transform.position += s;
+          if(Physics.Raycast(Selection.activeGameObject.transform.position, s, 1) == false){
+                    Selection.activeGameObject.transform.position += s;
           }
       }
     }
--- Assets/Scripts/ObjectClicker.cs
+++ Assets/Scripts/ObjectClicker.cs
@@ -1,31 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class ObjectClicker : MonoBehaviour
 {
 
-  public static GameObject CurObject = null;
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-          RaycastHit hit;
-          Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-          if (Physics.Raycast(ray, out hit, 100.0f)) {
-            if (hit.transform != null && hit.transform.gameObject.tag != "Schachbrett") {
-              CurObject = hit.transform.gameObject;
-              //PrintName(hit.transform.gameObject);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null && hit.transform.gameObject.tag != "Schachbrett")
+                {
+                    Selection.activeObject = hit.transform.gameObject;
+                }
             }
-          }
-          if(hit.transform.gameObject.tag == "Schachbrett"){
-            CurObject = null;
-          }
-        }
-    }
+            if (hit.transform.gameObject.tag == "Schachbrett")
+            {
 
-    private void PrintName(GameObject go){
-      print(go.name);
+            }
+        }
     }
 }
